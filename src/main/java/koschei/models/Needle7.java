d96539f13d406346423fb36d688f5a3b3382bcca
AlexBissing <avbissing@gmail.com>
2022-01-16 16:54:26 +0300

    all done

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -6,6 +6,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class Needle7 {
 
+    private Deth8 deth;
+
+    @Autowired
+    public Needle7(Deth8 deth) {
+        this.deth = deth;
+    }
+
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
